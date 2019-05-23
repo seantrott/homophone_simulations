81bb4e89dd344217fc0ad01063fb2ff2f4c98188
Sean Trott <seantrott@Seans-MacBook-Pro-8.local>
2019-05-23 12:35:14 -0700

    log

--- run_minimal.sh
+++ run_minimal.sh
@@ -1,5 +1,5 @@
 nohup python3 src/minimal_pairs.py &
 echo 'Beginning process:' > logs/mp_status.txt
-echo $! > logs/mp_status.txt
+echo $! >> logs/mp_status.txt
 wait
 echo 'All background processes have finished.' >> logs/mp_status.txt
