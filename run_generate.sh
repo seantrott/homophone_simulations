46bdb1501f2c9d40d5517dff1382b416522792a6
Sean Trott <trott.sean@gmail.com>
2019-09-23 13:04:02 -0700

    Updated shell script

--- run_generate.sh
+++ run_generate.sh
@@ -1,1 +1,5 @@
 nohup python3 src/generate_artificial_lexicon.py &
+echo 'Beginning process:' > logs/homophone_status.txt
+echo $! >> logs/homophone_status.txt
+wait
+echo 'All background processes have finished.' >> logs/homophone_status.txt
