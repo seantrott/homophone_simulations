3d351539a061bd65c03308d3c0e801b48e409396
Sean Trott <trott.sean@gmail.com>
2019-11-08 08:42:41 -0800

    minimal pairs update

--- src/minimal_pairs.py
+++ src/minimal_pairs.py
@@ -25,7 +25,7 @@ def find_minimal_pairs(wordforms, counts):
 	with tqdm(total=unique_combos) as progress_bar:
 		for w1, w2 in itertools.combinations(wordforms, 2):
 			# if len(w1) == len(w2) and ed.eval(w1, w2) == 1:
-			# Don't require two words to be the same length? Depends on operationalization of minimal pairs. Maybe count both.
+			# "Minimal pair" is defined here as wordforms 1 distance away. .
 			if ed.eval(w1, w2) == 1:
 				word_to_size[w1] += 1
 				word_to_size[w2] += 1
