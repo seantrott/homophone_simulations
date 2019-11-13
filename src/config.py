551a167e4d040477d5a73947fd9ade6727e06251
Sean T Trott <sttrott@ssrde.ucsd.edu>
2019-11-13 09:37:28 -0800

    japanese minimal pairs

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 """Config file."""
 
 
-LANGUAGE = 'german' # 
+LANGUAGE = 'japanese' # 
 
 LEXICON_PATHS = {'english': ['data/raw/english/celex_all.csv', '\\'],
 				 'french': ['data/raw/french/french_lexique.txt', '\t'],
