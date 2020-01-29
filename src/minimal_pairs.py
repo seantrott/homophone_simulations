c6fcc41df90af014cfac368e9c7fe4c38e8c3999
Sean Trott <trott.sean@gmail.com>
2020-01-29 11:11:50 -0800

    minimal pairs script

--- src/minimal_pairs.py
+++ src/minimal_pairs.py
@@ -69,22 +69,22 @@ def mps_for_artificials(df_arts, N):
 	return df_all_arts
 
 
-def main(language, N, matched, mp_dir, phon_column="PhonDISC"):
+def main(language, N, matched, mp_dir, phon_column="PhonDISC", nphones=5):
 	"""Main script."""
 
 	## Load files
 	dir_path = "data/processed/{lan}".format(lan=language)
-	art_string = "{lan}_artificial_{N}_matched_on_{match}_no_restriction.csv".format(lan=language, N=N, match=matched)
+	art_string = "{lan}_artificial_{N}_matched_on_{match}_no_restriction_{n}phone.csv".format(lan=language, N=N, match=matched, n=nphones)
 	artificial_path = op.join(dir_path, art_string)
 
 	# Load lexicons
-	df_real = pd.read_csv(op.join(dir_path, "{lan}_all_reals.csv".format(lan=language)))
+	df_real = pd.read_csv(op.join(dir_path, "{lan}_all_reals_{n}phone.csv".format(lan=language, n=nphones)))
 	df_artificials = pd.read_csv(artificial_path)
 
 	# Get minimal pairs for real lexicon
 	print("Getting minimal pairs for real lexicon...")
 	df_real_mps = mps_for_lexicon(df_real, phon_column=phon_column)
-	df_real_mps.to_csv("{dir}/{lan}_all_mps.csv".format(dir=mp_dir, lan=language))
+	df_real_mps.to_csv("{dir}/{lan}_all_mps_{n}phone.csv".format(dir=mp_dir, lan=language, n=nphones))
 
 	# Get minimal pairs for artificials
 	print("Getting minimal pairs for artificial lexicons...")
@@ -101,6 +101,7 @@ if __name__ == "__main__":
 	N = config.ITERATIONS
 	matched = config.MODEL_INFO['match_on']
 	phon_column = config.PHON_COLUMN[language]
+	nphones = config.MODEL_INFO['n']
 
 	## Check for directories
 	mp_dir = "data/processed/{lan}/minimal_pairs".format(lan=config.LANGUAGE)
@@ -109,7 +110,7 @@ if __name__ == "__main__":
 	    os.mkdir(mp_dir)
 
 	# Run main script
-	main(language=language, N=N, matched=matched, mp_dir=mp_dir, phon_column=phon_column)
+	main(language=language, N=N, matched=matched, mp_dir=mp_dir, phon_column=phon_column, nphones=nphones)
 
 
 
